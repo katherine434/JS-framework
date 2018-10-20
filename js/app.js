a7a6a93f525268bdd7746a4e20d67ef188fd9cfc
katherine434 <katherine.tr@outlook.com>
2018-10-19 19:43:03 -0500

    inicio

---
+++ js/app.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+
+	//Animación titulo
+	var i = 0;
+	while(i < 9000 ){
+		
+		$(".main-titulo").animate({color:"blue"}, 1000);		
+		$(".main-titulo").animate({color:"yellow"}, 1000);
+		i++;		
+	}
+
+
+	// Dulces down
+	function randomRuta(){
+		var num = (Math.floor(Math.random()*3)+1);
+		var ruta = "image/" + num + ".png";
+		return ruta;
+	};
+
+	//primera col
+
+	$(".col-1").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-1").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-1").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-1").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-1").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-1").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+
+
+	//segunda col
+
+	$(".col-2").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-2").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-2").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-2").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-2").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-2").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+
+
+	//tercera col
+
+	$(".col-3").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-3").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-3").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-3").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-3").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-3").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+
+
+	//cuarta col
+
+	$(".col-4").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-4").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-4").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-4").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-4").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-4").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+
+	//quinta col
+
+	$(".col-5").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-5").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-5").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-5").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-5").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-5").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+
+	//sexta col
+
+	$(".col-6").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-6").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-6").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-6").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-6").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-6").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+
+	//septima col
+
+	$(".col-7").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-7").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-7").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-7").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-7").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+	$(".col-7").prepend("<img src="+ randomRuta() +" style='width:112px;'>")
+
+
+
+
+});
