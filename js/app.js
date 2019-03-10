ad17f2ef42aa86acd0f7823d7e28eba7675c5f13
katherine434 <katherine.tr@outlook.com>
2019-03-10 11:56:19 -0500

    mejora matriz y nuevo ciclo
    .

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-	var arrayDulces = new Array(7);
-	var i = 0;	
+
+	var arrayDulces = new Array(6),
+		i = 0;
 
 	//Animación titulo
 	while(i < 9000 ){
@@ -8,26 +9,29 @@ $(document).ready(function(){
 		$(".main-titulo").animate({color:"blue"}, 1000);		
 		$(".main-titulo").animate({color:"yellow"}, 1000);
 		i++;		
-	}
+	}//---
 
+	// Creación matriz y asignar valor a cada elemento 
+	for(var a = 0; a < 6; a++){
+		arrayDulces[a] = new Array(7); // Nombre
+	}
 	
-
-	for(var a = 0; a< 7; a++){
-		arrayDulces[a] = new Array(7);
-	}
-
-	for (var b = 1; b < 7 ; b++) {
-		for (var c = 1; c <= 7 ; c++) {
+	for (var b = 0; b < 6 ; b++) {
+		for (var c = 0; c <= 7; c++) {
 			var creacionDulces = $(".col-"+c).prepend("<img src="+ randomRuta() +" style='width:112px'; class='dragdrop'>");
+
+			arrayDulces[b][c] = creacionDulces;
 		}
 	}
+	//console.log(arrayDulces[0][2]);
+	//console.log(arrayDulces[3][2]); --
 	
-
+	// -- Intercambio de dulces
 	$(".dragdrop").draggable({
 
 	 	revert: true,
 	 	cursorAt: {top: 56, left: 56}
-	 }) 
+		}) 
 
 	.droppable({
 		accept: ".dragdrop",
@@ -41,11 +45,23 @@ $(document).ready(function(){
 			$(this).attr("src", drag);
 			$(ui.draggable).attr("src", drop); 
 		}
-	})
+	}) // --
 
+
+// -- Intento quitar dulces grupos mayores a 3
+
+for (var f = 0; f < 6 ; f++) {
+	for (var g = 0; g <= 7; g++) {
+		
+		if ((arrayDulces[f][g]) === (arrayDulces[f][g])){
+			console.log("hola")
+		}else{
+			console.log("hola2")
+		}
+	}
+}
 	
-
-	// Dulces down
+// -- función para entregar ruta aleatoria dulces
 	function randomRuta(){
 		var num = (Math.floor(Math.random()*3)+1);
 		var ruta = "image/" + num + ".png" ;
