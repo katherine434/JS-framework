70412b7b319307f95e396bbcc346cac49b73296a
katherine434 <katherine.tr@outlook.com>
2019-03-14 22:02:21 -0500

    eliminar coincidencias

--- js/app.js
+++ js/app.js
@@ -18,20 +18,69 @@ $(document).ready(function(){
 	
 	for (var b = 0; b < 6 ; b++) {
 		for (var c = 0; c <= 7; c++) {
-			var creacionDulces = $(".col-"+c).prepend("<img src="+ randomRuta() +" style='width:112px'; class='dragdrop'>");
 
-			arrayDulces[b][c] = creacionDulces;
+			var creacionDulces = $(".col-"+c).prepend("<img src="+ randomRuta()+" style='width:112px'; class='dragdrop'; id ='fila-"+ b +"colum-"+ c +"'>");
+
+			arrayDulces[b][c] = creacionDulces;			
 		}
 	}
 	//console.log(arrayDulces[0][2]);
 	//console.log(arrayDulces[3][2]); --
-	
+
+	//var variable =  $("#fila-3colum-3").attr("src");
+	//console.log(variable)
+	// -- Intento quitar dulces grupos mayores a 3
+
+
+	for (var f = 0; f < 6 ; f++) {
+		for (var g = 0; g <= 7; g++) {			
+
+			var valor = $("#fila-"+ f + "colum-"+ g ).attr("src") 
+			var val1 = $("#fila-"+ (f - 1) + "colum-"+ g).attr("src")
+			var val2 = $("#fila-"+ f +"colum-" + (g + 1)).attr("src")
+			var val3 = $("#fila-"+ (f + 1) + "colum-"+ g).attr("src")
+			var val4 = $("#fila-"+ f +"colum-" + (g - 1)).attr("src")
+
+			//console.log(val1, val2, val3, val4)
+			if(((f - 1) < 0 ) && ((g - 1) < 0)){
+				
+				f = 0;
+				g = 0;
+
+				//console.log("se convirtio ", val1, val2, val3, val4)
+			}else{
+				//console.log("no hubo necesidad ", val1, val2, val3, val4)
+				if((val1 == valor) && (valor == val3)){
+
+			 		//console.log("hay filas de 3")
+			 		$("#fila-"+ (f - 1) + "colum-"+ g).remove();
+			 		$("#fila-"+ f + "colum-"+ g ).remove();
+			 		$("#fila-"+ (f + 1) + "colum-"+ g).remove();
+				}else{  
+			 		console.log("no hay coincidencias")
+				}
+
+				if((val2 == valor) && (valor == val4)){
+
+					$("#fila-"+ f +"colum-" + (g + 1)).remove();
+					$("#fila-"+ f + "colum-"+ g ).remove();
+					$("#fila-"+ f +"colum-" + (g - 1)).remove();
+			 		
+				}else{  
+			 		console.log("no hay coincidencias")
+			 	}
+			}
+		}			
+	}
+
+
+
 	// -- Intercambio de dulces
 	$(".dragdrop").draggable({
 
 	 	revert: true,
 	 	cursorAt: {top: 56, left: 56}
-		}) 
+	}) 
 
 	.droppable({
 		accept: ".dragdrop",
@@ -47,24 +96,11 @@ $(document).ready(function(){
 		}
 	}) // --
 
-
-// -- Intento quitar dulces grupos mayores a 3
-
-for (var f = 0; f < 6 ; f++) {
-	for (var g = 0; g <= 7; g++) {
-		
-		if ((arrayDulces[f][g]) === (arrayDulces[f][g])){
-			console.log("hola")
-		}else{
-			console.log("hola2")
-		}
-	}
-}
 	
 // -- función para entregar ruta aleatoria dulces
 	function randomRuta(){
 		var num = (Math.floor(Math.random()*3)+1);
-		var ruta = "image/" + num + ".png" ;
+		var ruta = "image/" + num + ".png";
 		return ruta
 	};
 
